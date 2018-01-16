3b8b2c70ea2972956b1cc47854c56769622be815
Dale S Greve <dale@dalegreve.com>
2018-01-15 20:46:46 -0500

    udpated footer and form

--- footer.php
+++ footer.php
@@ -2,12 +2,16 @@
 </div>
 <footer id="footer" role="contentinfo">
 <div id="copyright">
-<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'blankslate' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); ?>
+<?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'blankslate' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); ?> | <a href="/privacy-policy">Privacy Policy</a>
 </div>
 <section="attribution">
-<div>Icons made by <a href="//www.flaticon.com/authors/becris" title="Becris">Becris</a>, <a href="//www.flaticon.com/authors/prosymbols" title="Prosymbols">Prosymbols</a>, <a href="//www.flaticon.com/authors/eucalyp" title="Eucalyp">Eucalyp</a>,<a href="//www.flaticon.com/authors/nhor-phai" title="Nhor Phai">Nhor Phai</a> and <a href="//www.freepik.com" title="Freepik">freepik</a> from <a href="//www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="//creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
-
-  <div>Icons made by  from <a href="//www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="//creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+<div>
+  Icons made by <a href="//www.flaticon.com/authors/becris" title="Becris">Becris</a>,
+  <a href="//www.flaticon.com/authors/prosymbols" title="Prosymbols">Prosymbols</a>,
+  <a href="//www.flaticon.com/authors/eucalyp" title="Eucalyp">Eucalyp</a>,
+  <a href="//www.flaticon.com/authors/nhor-phai" title="Nhor Phai">Nhor Phai</a> and 
+  <a href="//www.freepik.com" title="Freepik">freepik</a> from <a href="//www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="//creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a>
+</div>
 </section>
 </footer>
 </div>
