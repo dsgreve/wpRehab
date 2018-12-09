cca317c8bbeeade9454cc5a727e2d23d646d22c0
Dale Greve <dale@dalegreve.com>
2018-12-09 11:12:33 -0500

    removing all files to start fresh with Gulp

--- functions.php
+++
@@ -1,9 +0,0 @@
-<?php
-/************************************************************************
-wordpressRehab functions file
-**********************************************************************/
-function wpRehab_style(){
-  wp_enqueue_style( 'bootstrap' . 'https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css' );
-}
-
-add_action( 'wp_enqueue_scripts', 'wpRehab_style' );
