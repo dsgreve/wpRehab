dfa9a14099d85cf9b4e28a2677dcd39eba752d9b
Dale Greve <dale@dalegreve.com>
2018-12-19 20:11:14 -0500

    hooked in parent theme

--- functions.php
+++ functions.php
@@ -1,24 +1,31 @@
 <?php
 /**
- * Functions
+ * Custom Wordpress Rehab Theme.
  *
- * @package      WordpressRehab
- * @author       Dale Greve <dale@dalegreve.com>
- * @copyright    Copyright (c) 2018, Dale Greve
- * @license      http://opensource.org/licenses/gpl-2.0.php GNU Public License
+ * This file adds functions to the wpRehab Theme.
  *
+ * @package wpRehab
+ * @author  Dale Greve
+ * @license GPL-2.0-or-later
+ * @link    https://www.dalegreve.com/
  */
 
-/**
- * Theme Setup
- *
- * This setup function attaches all of the site-wide functions 
- * to the correct actions and filters. All the functions themselves
- * are defined below this setup function.
- *
- */
+include_once( get_template_directory() . '/lib/init.php' );
 
-add_action('genesis_setup','child_theme_setup', 15);
-function child_theme_setup() {
+//* We tell the name of our child theme
+define( 'Child_Theme_Name', __( 'WordpresRehab', 'wprehab' ) );
+//* We tell the web address of our child theme (More info & demo)
+define( 'Child_Theme_Url', 'http://dalegreve.com' );
+//* We tell the version of our child theme
+define( 'Child_Theme_Version', '2.1.0' );
 
+//* Add HTML5 markup structure from Genesis
+add_theme_support( 'html5' );
+
+//* Add HTML5 responsive recognition
+add_theme_support( 'genesis-responsive-viewport' );
+
+add_action( 'wp_enqueue_scripts', 'my_child_theme_scripts' );
+function my_child_theme_scripts() {
+    wp_enqueue_style( 'parent-theme-css', get_template_directory_uri() . '/style.css' );
 }
---
+++ homepage.php
@@ -0,0 +1,11 @@
+/**
+ * This file adds the Custom Landing template to the Metro Theme.
+ *
+ * @author Dale Greve
+ * @package wpRehab
+ * @subpackage Customizations
+ */
+
+/*
+Template Name: Homepage
+*/
