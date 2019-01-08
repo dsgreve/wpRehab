894e133a1bed6ecc45e871d98956026b40d397fe
Dale Greve <dale@dalegreve.com>
2019-01-07 22:53:44 -0500

    added waypoint services section

--- src/scripts/modules/RevealOnScroll.js
+++ src/scripts/modules/RevealOnScroll.js
@@ -1,8 +1,28 @@
 import $ from 'jquery';
+import waypoints from '../../../node_modules/waypoints/lib/noframework.waypoints';
 
 class RevealOnScroll {
     constructor() {
-        this.itemsToReveal = (".service-item");
+        this.itemsToReveal = $(".service-item .wp-block-column");
+        this.hideInitially();
+        this.createWaypoints();
+    }
+
+    hideInitially() {
+        this.itemsToReveal.addClass("reveal-item");
+    }
+
+    createWaypoints() {
+        this.itemsToReveal.each(function(){
+            const currentItem = this;
+            new Waypoint({
+                element: currentItem, //dom element we watch
+                handler: function() {
+                    $(currentItem).addClass("reveal-item--is-visible")
+                }, //what we want to run
+                offset: "86%"
+            });
+        });
     }
 }
 
