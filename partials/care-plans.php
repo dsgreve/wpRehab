fcdbd87bb59853c71090bebe4580fd86595f6c14
Dale Greve <dale@dalegreve.com>
2019-05-26 09:26:16 -0400

    updated styling for hero and care plans

--- partials/care-plans.php
+++ partials/care-plans.php
@@ -1,50 +1,58 @@
 <section>
-<div class="container">
-    <div id="plan-table" class="row row--gutters-small row--equal-height-at-large row--py-60">
-    <div class="row__medium-4">
-    <div class="plan">
-        <h3>Starter<span>$137</span></h3>
-        <p>Monthly</p>
-                
-        <ul>
-            <li>Site Backups and restorations</li>
-            <li>Up to <strong>3 hours</strong> of custom updates</li>
-            <li>Theme, plugin, and core software updates</li>
-            <li>Monthly Site Performance Reports</li>
-			<li>Website Uptime monitoring</li>			
-        </ul>
-        <a class="signup" href="https://wordpressrehab.com/#contact-form">Get Starter Service</a> 
+    <div class="container">
+        <div id="plan-table" class="row row--gutters-small row--equal-height-at-medium row--py-60">
+            <div class="row__medium-4">
+                <div class="plan">
+                    <h3>Starter<span>$137</span></h3>
+                    <p>Monthly</p>
+                            
+                    <ul>
+                        <li>Site Backups and restorations</li>
+                        <li>Up to <strong>3 hours</strong> of custom updates</li>
+                        <li>Theme, plugin, and core software updates</li>
+                        <li>Monthly Site Performance Reports</li>
+                        <li>Website Uptime monitoring</li>			
+                    </ul>
+                    <div class="plan__button">
+                        <a class="signup" href="https://wordpressrehab.com/#contact-form">Get Starter Service</a>
+                    </div> 
+                </div>
+            </div>
+            <div class="row__medium-4">
+                <div class="plan">
+                    <h3>Concierge<span>$275</span></h3>
+                    <p>Monthly</p>             
+                    <ul>
+                        <li>Site Backups and restorations</li>
+                        <li>Up to <strong>6 hours</strong> of custom updates</li>
+                        <li>Theme, plugin, and core software updates</li>
+                        <li>Monthly Site Performance Reports</li>
+                        <li>Monthly Analytics/SEO Reports</li>
+                        <li>Website Uptime monitoring</li>			
+                    </ul>
+                    <div class="plan__button">
+                        <a class="signup" href="https://wordpressrehab.com/#contact-form">Get Concierge Service</a>
+                    </div>
+                </div>
+            </div>
+            <div class="row__medium-4">
+                <div id="customPlan" class="plan">
+                    <h3>Custom Plan<span>Free Quote</span></h3>
+                    <p>&nbsp;</p> 
+                            
+                    <p>We can work together on to develop a plan to fit your business needs. This plan is for customers that are looking for a full service solution.  </p>
+                        <ul>
+                            <li>A/B testing</li>
+                            <li>Landing Page Optimization</li>
+                            <li>Template Customizations</li>
+                            <li>Website Redesigns</li>
+                        </ul>
+                    <div class="plan__button">
+                        <a class="signup" href="https://wordpressrehab.com/#contact-form">Get your Free Quote</a>
+                    </div>
+                </div>  
+            </div>
+        </div>
     </div>
-    </div>
-    <div class="row__medium-4">
-    <div class="plan">
-        <h3>Concierge<span>$275</span></h3>
-        <p>Monthly</p>             
-        <ul>
-            <li>Site Backups and restorations</li>
-            <li>Up to <strong>6 hours</strong> of custom updates</li>
-            <li>Theme, plugin, and core software updates</li>
-            <li>Monthly Site Performance Reports</li>
-            <li>Monthly Analytics/SEO Reports</li>
-			<li>Website Uptime monitoring</li>			
-        </ul>
-        <a class="signup" href="https://wordpressrehab.com/#contact-form">Get Concierge Service</a>
-    </div>
-    </div>
-    <div class="row__medium-4">
-    <div id="customPlan" class="plan">
-        <h3>Custom Plan<span>Free Quote</span></h3>
-        <p>&nbsp;</p> 
-                 
-        <p>We can work together on to develop a plan to fit your business needs. This plan is for customers that are looking for a full service solution.  </p>
-            <ul>
-                <li>A/B testing</li>
-                <li>Landing Page Optimization</li>
-                <li>Template Customizations</li>
-                <li>Website Redesigns</li>
-            </ul>
-        <a class="signup" href="https://wordpressrehab.com/#contact-form">Get your Free Quote</a>
-    </div>
-    </div>
-    </div>
+
 </section>
---
+++ src/js/modules/EqualHeights.js
@@ -0,0 +1,16 @@
+import $ from 'jquery';
+console.log('height loaded')
+
+class EqualHeights  {
+    constructor() {
+        $('#plan-table ul');
+        this.setHeights();
+    }
+
+
+    setHeights() {
+        console.log('get height')
+    }
+}
+
+export default EqualHeights;
